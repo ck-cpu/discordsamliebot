c95de502170fa8c710ed4ab90f889edfdd755e1d
ck-cpu <kevinsh525@gmail.com>
2021-02-15 20:30:29 +0900

    ㅇㅇ

--- index.js
+++ index.js
@@ -58,10 +58,33 @@ client.on('message', async message => {
   if (message.content === '$샘플 악식킹 1') {
   const embed = new Discord.MessageEmbed()
   .setTitle('약점보험 악식킹 샘플 1')
-  .setDescription('**포켓몬**: 악식킹\n**특성**: 비스트부스트\n**성격**: 냉정\n**도구**: 약점보험\n**노력치**: B 252/D 252/H 6\n**기술**: 악의파동/용성군/불대문자/타점 좋은 기술\n\n**추가 설명**: 이렇게 내구 넣으면 물리내구 7만 6천 특수내구 7만 6천입니다.\n다맥하면 따라큐 치근거리기 버팀\n그리고 트릭룸 요원 꼭 필요합니다.') 
+  .setDescription('**포켓몬**: 악식킹\n**특성**: 비스트부스트\n**성격**: 냉정\n**도구**: 약점보험\n**노력치**: B 252/D 252/H 6\n**기술**: 악의파동/용성군/불대문자/오물폭탄 또는 오물웨이브\n\n**추가 설명**: 이렇게 내구 넣으면 물리내구 7만 6천 특수내구 7만 6천입니다.\n다맥하면 따라큐 치근거리기 버팀\n그리고 트릭룸 요원 꼭 필요합니다.') 
   .setColor('BLACK') 
   .setFooter(`샘플의 관한 질문은 질문 채널에서 해주시면 감사하겠습니다.`)
   embed.setImage('https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0799_000_uk_n_00000000_f_n.png')
+  message.channel.send(embed)
+  }
+})
+
+client.on('message', async message => {
+  if (message.content === '$샘플 폴리곤2') {
+  const embed = new Discord.MessageEmbed()
+  .setTitle('현재 `폴리곤2` 샘플은 1개 있어요!')
+  .setDescription('**$샘플 폴리곤 (번호)를 써주시면 볼 수 있습니다!**') 
+  .setColor('#E3DDCB') 
+  .setFooter(`EX) $샘플 시드라 1`)
+  message.channel.send(embed)
+  }
+}) 
+
+client.on('message', async message => {
+  if (message.content === '$샘플 폴리곤2 1') {
+  const embed = new Discord.MessageEmbed()
+  .setTitle('진화의휘석 폴리곤2 샘플 1')
+  .setDescription('**포켓몬**: 폴리곤\n**특성**: 다운로드\n**성격**: 조심\n**도구**: 진화의휘석\n**노력치**: H 252 | B 252\n**개체값: A, S 제외 4V**기술**: 10만볼트/트릭룸/트라이어택/사이코키네시스\n\n**추가 설명**: 폴리곤2 h, b만 넣어도 무릎차기 에 확2타 (생명의구슬 이어도)\n**같이 쓰면 좋은 포켓몬**: 차곡차곡') 
+  .setColor('#E3DDCB') 
+  .setFooter(`샘플의 관한 질문은 질문 채널에서 해주시면 감사하겠습니다.`)
+  embed.setImage('https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0233_000_uk_n_00000000_f_n.png')
   message.channel.send(embed)
   }
 })
